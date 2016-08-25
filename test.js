859112ab5b79e059993fe1d2c3c3126a74683179
Anh Le Hoang <lehoanganh25991@gmail.com>
2016-08-25 14:01:49 +0700

    click to [link with text], '=texthere'

--- test.js
+++ test.js
@@ -4,10 +4,12 @@ var options = {
     port: 4723,
     desiredCapabilities: {
         app: 'browser',
-        // app: 'latest_dev.apk',
         browserName: 'chrome',
+        // app: 'D:\\work-station\\webdriverio-test\\latest_dev.apk',
+        // autoLaunch: true,
         platformName: 'android',
-        deviceName: 'abc'
+        deviceName: 'cutepad TX_A7133_1508_',
+
     }
 };
  
@@ -15,14 +17,18 @@ var client = webdriverio.remote(options);
 
 client
     .init()
-    .url('https://duckduckgo.com/')
-    .setValue('#search_form_input_homepage', 'WebdriverIO')
-    .click('#search_button_homepage')
-    .getTitle()
-    .then(function(title) {
-        console.log('Title is: ' + title);
+    .url('https://rightfrom.us/beta/hoipos/')
+    // .setValue('#search_form_input_homepage', 'WebdriverIO')
+    // .selectByVisibleText('.widget-user-username > a', 'latest_staging.apk')
+    .click('=latest_staging.apk')
+    // .getTitle()
+    // .then(function(title) {
+    //     console.log('Title is: ' + title);
 
-        // outputs:
-        // "Title is: WebdriverIO (Software) at DuckDuckGo"
-    })
+    //     // outputs:
+    //     // "Title is: WebdriverIO (Software) at DuckDuckGo"
+    // })
+    // .installApp('D:\\work-station\\webdriverio-test\\latest_dev.apk', function(){
+    // 	console.log('install app');
+    // })
     .end();
