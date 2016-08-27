b1cc1c2e0e83e27560de343f60faa7e8e7515232
Anh Le Hoang <lehoanganh25991@gmail.com>
2016-08-28 06:17:03 +0700

    using mocha programmatically, runner's e  vent

--- using-mocha-v1.js
+++ using-mocha-v1.js
@@ -1,22 +1,95 @@
-var Mocha = require('mocha'),
-	fs = require('fs'),
-	path = require('path');
+var util = require('util');
+var Mocha = require('mocha');
 
 // Instantiate a Mocha instance.
-var mocha = new Mocha();
+var mocha = new Mocha({});
 
 mocha.addFile('test.js');
 
-// mocha.reporter('list');
-mocha.reporter('ti-reporter');
+// mocha.reporter('ti-reporter');
 
-// Run the tests.
-var runner = mocha.run(function(failures){
-	process.on('exit', function(){
-		process.exit(failures);  // exit with non-zero status if there were failures
-		console.log('proccess.log', process.log);
-	});
-});
 
-console.log('runner', runner);
 
+var passes = 0;
+var failures = 0;
+
+var log = {
+	status: false,
+	content: ''
+};
+
+// // Run the tests.
+// mocha.run()
+// 	.on('pass', function(test){
+// 		passes++;
+// 		log.content += util.format('Pass: %s\n', test.fullTitle());
+// 	})
+
+// 	.on('fail', function(test, err){
+// 		failures++;
+// 		log.content += util.format('Fail: %s\n-- error: %s\n', test.fullTitle(), err.message);
+// 	})
+
+// 	.on('end', function(){
+// 		log.content += util.format('\nEnd: %d/%d\n', passes, passes + failures);
+
+// 		var isAllPassed = passes == (passes + failures);
+
+// 		log.status = isAllPassed;
+
+// 		// console.log(log.content);
+
+// 		// runner.log = process.log;
+		
+// 		process.exit(failures);
+// 	});
+
+// var sendNotification = require('slack');	
+
+// var attachmentTxt = "<https://github.com/hoanganh25991/appium-wd-test/blob/master/wd-test-android-basic-work.js|test on pos.hoicard>";
+
+
+mocha.run()
+    .on('pass', function(test) {
+        passes++;
+    })
+    .on('fail', function(test, err) {
+        failures++;
+        log.content += util.format('--------\nTest title: %s\nTest duration: %sms\n%s', test.title, test.duration, (err + '\n'));
+    })
+    .on('end', function() {
+        var summary = util.format('Passes/Total: %d/%d', passes, (passes + failures));
+
+        var consoleOutput = log.content;
+
+        var isAllPassed = passes == (passes + failures);
+
+		var color = isAllPassed ? "good" : "danger";
+
+		var attachmentTxt = "<https://github.com/hoanganh25991/appium-wd-test/blob/master/wd-test-android-basic-work.js|Test on pos.hoicard>";
+
+		var attachments = {
+			"attachments": [
+				{
+					"fallback": "Test on pos.hoicard",
+					"text": attachmentTxt,
+					"fields": [
+						{
+							"title": summary,
+							"value": consoleOutput,
+							"short": true
+						}
+					],
+					"color": color
+				}
+			]
+		};
+
+		var sendNotification = require('./helpers/slack');	
+
+		sendNotification(attachments);
+
+    })
+    .on('exit', function(failures){
+    	process.exit(failures);
+    });
